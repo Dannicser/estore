d84303a39d2b25416cda07d57f3c4ee423f6a9c2
Danil Dmitriev <danil.dmitriev.2014@inbox.ru>
2025-03-11 00:12:20 +0300

    fix

--- vue.config.js
+++ vue.config.js
@@ -3,10 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()], // Подключаем плагин Vue
-  base: process.env.NODE_ENV === 'production' ? '/e-store/' : '/e-store', // Указываем базовый путь
+  base: process.env.NODE_ENV === 'production' ? '/estore/' : '/estore', // Указываем базовый путь
   build: {
     outDir: 'dist', // Папка для сборки
-    assetsDir: '/e-store/assets', // Папка для статических файлов (CSS, изображения и т.д.)
+    assetsDir: '/estore/assets', // Папка для статических файлов (CSS, изображения и т.д.)
     emptyOutDir: true // Очищать папку перед сборкой
   }
 })
