3a0e3ff0df7205a0830732dc68d0a9a0942e492f
Danil Dmitriev <danil.dmitriev.2014@inbox.ru>
2025-03-15 23:46:43 +0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,3 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -7,10 +5,5 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  base: '/estore/'
+  base: process.env.NODE_ENV === 'production' ? '/estore/' : '/'
 })
